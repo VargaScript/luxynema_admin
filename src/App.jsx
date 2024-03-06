fb28d9e6e5c119776f9c95bfdb80eb8b7bf656ec
VargaScript <wolfolkvargas295478@gmail.com>
2024-03-06 00:13:09 -0700

    Initial Configuration Ended

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@material-tailwind/react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <p>Luxynema Admin</p>
+      <Button>Button</Button>
     </>
   )
 }
