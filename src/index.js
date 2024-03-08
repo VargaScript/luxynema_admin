cc91dc821023a4d667f01a5f6e9d420316220dd0
JhoanaC <abicor339@gmail.com>
2024-03-08 16:21:43 -0700

    routeur styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import routeurApp from './routeur/routeurApp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
+
+ReactDOM.render(<routeurApp />, document.getElementById('root'));
