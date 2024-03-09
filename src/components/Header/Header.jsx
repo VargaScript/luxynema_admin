e14d72e70a46d347906e05f290ea29bf7e880f75
Yahir Morfin <144406875+yahirmxx@users.noreply.github.com>
2024-03-08 18:29:14 -0600

    cambios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,122 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  List,
+  ListItem,
+  ListItemPrefix,
+  Avatar,
+  Card,
+  Typography,
+} from "@material-tailwind/react";
 
 export const Header = () => {
   return (
-    <header>
-      <h1>Aplicación de Administración de Libros</h1>
-      <hr />
-      <div className="links">
-        <NavLink to="/" className="link" activeClassName="active" exact>
-          Lista de Libros
-        </NavLink>
-        <NavLink to="/add" className="link" activeClassName="active">
-          Agrega Libro
-        </NavLink>
+    <>
+      <div className="container">
+        <Typography variant="h1" color="white" className="text-center">
+          Administrar Luxynema
+        </Typography>
+        <div className="flex flex-wrap justify-center">
+          <Card className="w-96 mr-4 mb-4">
+            <List>
+              <ListItem>
+                <ListItemPrefix>
+                  <Avatar
+                    variant="circular"
+                    alt="candice"
+                    src="https://docs.material-tailwind.com/img/face-1.jpg"
+                  />
+                </ListItemPrefix>
+
+                <div>
+                  <Typography variant="h6" color="blue-gray">
+                    Añadir película
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal"
+                  >
+                    Añade la información de las películas listadas en la app
+                    Luxynema
+                  </Typography>
+                </div>
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <Avatar
+                    variant="circular"
+                    alt="alexander"
+                    src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  />
+                </ListItemPrefix>
+                <div>
+                  <Typography variant="h6" color="blue-gray">
+                    Ver películas
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal"
+                  >
+                    Lista, Actualiza, Elimina, las películas añadidas en la app
+                    Luxynema
+                  </Typography>
+                </div>
+              </ListItem>
+            </List>
+          </Card>
+
+          <Card className="w-96 ml-4 mb-4">
+            <List>
+              <ListItem>
+                <ListItemPrefix>
+                  <Avatar
+                    variant="circular"
+                    alt="candice"
+                    src="https://docs.material-tailwind.com/img/face-1.jpg"
+                  />
+                </ListItemPrefix>
+
+                <div>
+                  <Typography variant="h6" color="blue-gray">
+                    Añadir usuario
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal"
+                  >
+                    Añade los usuarios para que tengan acceso a Luxynema
+                  </Typography>
+                </div>
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <Avatar
+                    variant="circular"
+                    alt="alexander"
+                    src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  />
+                </ListItemPrefix>
+                <div>
+                  <Typography variant="h6" color="blue-gray">
+                    Ver usuarios
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal"
+                  >
+                    Lista, Actualiza, Elimina, los usuarios añadidos en la app
+                    Luxynema
+                  </Typography>
+                </div>
+              </ListItem>
+            </List>
+          </Card>
+        </div>
       </div>
-    </header>  )
-}
+    </>
+  );
+};
--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -71,64 +71,7 @@ export const MovieForm = (props) => {
   };
 
   const handleInputChange = (event) => {
-    const { nombre, valor } = event.target;
 
-    // Validación y actualización según el nombre del campo
-    switch (nombre) {
-      case "duracion":
-        // Validar que el valor sea un número entero positivo
-        if (
-          valor === "" ||
-          (parseInt(valor) === +valor && parseInt(valor) > 0)
-        ) {
-          setPelicula((prevState) => ({
-            ...prevState,
-            [nombre]: valor,
-          }));
-        }
-        break;
-      case "precio":
-        // Validar que el valor sea un número decimal positivo
-        if (
-          valor === "" ||
-          (valor.match(/^\d{1,}(\.\d{0,2})?$/) && parseFloat(valor) > 0)
-        ) {
-          setPelicula((prevState) => ({
-            ...prevState,
-            [nombre]: valor,
-          }));
-        }
-        break;
-      case "generos":
-        // No se realiza validación, se actualiza directamente
-        setPelicula((prevState) => ({
-          ...prevState,
-          [nombre]: valor,
-        }));
-        break;
-      case "horario":
-        // Validar que el valor tenga formato de hora
-        if (valor === "" || valor.match(/^(?:[0-1]\d|2[0-3]):[0-5]\d$/)) {
-          setPelicula((prevState) => ({
-            ...prevState,
-            [nombre]: valor,
-          }));
-        }
-        break;
-      case "img_url":
-      case "img_url_hd":
-      case "sinopsis":
-      case "titulo":
-        // No se realiza validación, se actualiza directamente
-        setPelicula((prevState) => ({
-          ...prevState,
-          [nombre]: valor,
-        }));
-        break;
-      default:
-        // Se ignora el campo no reconocido
-        break;
-    }
   };
   return (
     <>
